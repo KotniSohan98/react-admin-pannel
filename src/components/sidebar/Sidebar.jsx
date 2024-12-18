1414d6116cf93e63b7f7332f092193cab9b66f95
Kotni Sohan <kotnisohan09@gmail.com>
2024-12-18 22:48:18 +0530

    Installed Material UI Icons and added Icons

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import StoreIcon from "@mui/icons-material/Store";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import InsertChartSharpIcon from "@mui/icons-material/InsertChartSharp";
+import NotificationsNoneSharpIcon from "@mui/icons-material/NotificationsNoneSharp";
+import SettingsApplicationsSharpIcon from "@mui/icons-material/SettingsApplicationsSharp";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 const Sidebar = () => {
   return (
@@ -10,17 +22,58 @@ const Sidebar = () => {
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
+            <DashboardIcon />
             <span>Dashboard</span>
           </li>
+          <p className="title">LISTS</p>
           <li>
-            <span>Dashboard</span>
+            <PersonOutlineOutlinedIcon />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <StoreIcon />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <CreditCardIcon />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <InsertChartSharpIcon />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneSharpIcon />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplicationsSharpIcon />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlinedIcon />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppIcon />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
