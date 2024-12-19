3d90da1b554e3d6a4aa25326416ea598540904f2
KotniSohan98 <sohan.kotni@accenture.com>
2024-12-20 00:46:15 +0530

    Made widgets to a reusable component as per Temp data

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,21 +1,92 @@
 import "./widget.scss";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import AccountBalanceOutlinedIcon from "@mui/icons-material/AccountBalanceOutlined";
 
-const Widget = () => {
+const Widget = ({ type }) => {
+  //Temporary
+  const amount = 100;
+  const diff = 20;
+  let data;
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "see all users",
+        icon: (
+          <PersonOutlineOutlinedIcon
+            className="icon"
+            style={{ color: "crimson", backgroundColor: "rgba(255,0,0,0.2)" }}
+          />
+        ),
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "view all orders",
+        icon: (
+          <ShoppingCartOutlinedIcon
+            className="icon"
+            style={{
+              color: "goldenrod",
+              backgroundColor: "rgba(218,165,32,0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "view net users",
+        icon: (
+          <MonetizationOnOutlinedIcon
+            className="icon"
+            style={{ color: "green", backgroundColor: "rgba(0,128,0,0.2)" }}
+          />
+        ),
+      };
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "see details",
+        icon: (
+          <AccountBalanceOutlinedIcon
+            className="icon"
+            style={{ color: "purple", backgroundColor: "rgba(128,0,128,0.2)" }}
+          />
+        ),
+      };
+      break;
+
+    default:
+      break;
+  }
   return (
     <div className="widget">
       <div className="left">
-        <span className="title">USERS</span>
-        <span className="counter">21312</span>
-        <span className="link">see all users</span>
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"}
+          {amount}
+        </span>
+        <span className="link">{data.link}</span>
       </div>
       <div className="right">
         <div className="percentage positive">
           <KeyboardArrowUpIcon />
-          20%
+          {diff}%
         </div>
-        <PersonOutlineOutlinedIcon className="icon" />
+        {data.icon}
+        {/* <PersonOutlineOutlinedIcon className="icon" /> */}
       </div>
     </div>
   );
